fb84479ed108458f5db4859176f52236a07829e7
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-02 12:58:35 +0530

    Day 2 - Exercise 3: Lists & Dictionaries

---
+++ 01_data_types_and_operators/04__exercise_03.py
@@ -0,0 +1,20 @@
+# Create a list of 5 numbers
+# Create a dictionary with 3 items (anything you want)
+# Check if a value exists in the list using 'in'
+# Check if a key exists in the dictionary using 'in'
+# Do some math with two numbers from the list and store the result in a variable
+# Print everything
+
+l = [10, 20, 30, 40, 50]
+print("List of 5 numbers =",l)
+
+d = {"John": 1.1, "Kole": 2.2, "Banana": "Sweet"}
+print("Dictionary With 3 items:",d)
+
+print(f'is 20 in list = {20 in l}')
+print(f'is 21 in list = {21 in l}')
+
+print(f"is Rosy present in the Dictionary = {"Rosy" in d}")
+
+a = l[0] + l[1]
+print("Added 2 list items and stored them in a =",a)
