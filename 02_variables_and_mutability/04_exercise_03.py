db697b0b44770d5dda3527dccd8221fac2963832
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-05 12:09:23 +0530

    Day 4 - Exercise 3: List Operations

---
+++ 02_variables_and_mutability/04_exercise_03.py
@@ -0,0 +1,14 @@
+# Create a list of 5 cities
+# Change the 3rd city to a new one using index
+# Add a new city using append() - self research
+# Remove a city using remove() - self research
+# Print the list after each change
+
+l = ["London", "Sydney", "Mumbai", "Florida", "New York"]
+
+l[2] = "Edinburg"
+
+l.append("California")
+l.remove("London")
+
+print(l)
