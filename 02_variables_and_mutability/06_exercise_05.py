59b638e272baa1e7a28bbee5a7caf2912607e3d0
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-05 12:23:37 +0530

    Day 4 - Exercise 5: Dictionary Operations

---
+++ 02_variables_and_mutability/06_exercise_05.py
@@ -0,0 +1,14 @@
+# Create a dictionary of 3 products with name as key and price as value
+# Update the price of one product
+# Add a new product
+# Check if a product exists using 'in'
+# Print the final dictionary
+
+d = {"laptop": 99000, "pc": 250000, "smart phone": 40000}
+d["laptop"] = 120000
+
+d["TV"] = 87000
+
+print("TV" in d)
+
+print(d)
