ca755e296c1f669aa92762c4c425bd95e9cff3d1
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-07 12:05:17 +0530

    Day 6 - Beginner Project 1: Madlibs

---
+++ 04_beginner_project_madlibs/madlibs.py
@@ -0,0 +1,7 @@
+word = input("Word: ")
+
+place = input("Place: ")
+
+food = input("Food: ")
+
+print(f"Theres a big {word} whos located in {place} who tried to beat the {food} for no reason at all because {word} was hungry")
