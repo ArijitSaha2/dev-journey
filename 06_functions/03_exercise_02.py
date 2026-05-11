84791e97ce98f09ec57c9d73d32521eafd3d07fb
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-11 12:01:23 +0530

    Day 9 - Exercise 2: Math Functions

---
+++ 06_functions/03_exercise_02.py
@@ -0,0 +1,10 @@
+# Create a function that takes two numbers as arguments
+# Return the sum, difference, and product
+# Print all three results when calling the function
+
+def num(n1, n2):
+    return(f"Sum = {n1 + n2}, \nDifference = {n1 - n2}, \nProduct = {n1 * n2}")
+
+result = num(10, 5)
+
+print(result)
