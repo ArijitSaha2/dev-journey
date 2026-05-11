1ea8e2874550b700bad706d46bcb1d1a04028899
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-11 11:48:43 +0530

    Day 9 - Exercise 1: Functions with Arguments

---
+++ 06_functions/02_exercise_01.py
@@ -0,0 +1,10 @@
+# Create a function that takes a name and age as arguments
+# Print a greeting message using both
+# Call it 3 times with different values
+
+def nage(names, ages):
+    print(f"Greetings {names} and welcome!! Your {ages}!!!!")
+    
+nage("Ella", 22)
+nage("Smith", 29)
+nage("Invincible", 21)
