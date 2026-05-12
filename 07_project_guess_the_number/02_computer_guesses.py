beb79b7787c6ca65eaeb5b57e82784b32d1c79fc
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-12 13:27:43 +0530

    Day 10 - Beginner Project 4: Computer Guesses Your Number (Smart)

--- 07_project_guess_the_number/02_computer_guesses.py
+++ 07_project_guess_the_number/02_computer_guesses.py
@@ -1,25 +1,30 @@
 # User thinks of a number between 1 and 10
 # Computer guesses a number
 # User tells computer if it's too high, too low, or correct (via input)
-# Computer keeps guessing until it gets it right
+# Computer keeps guessing until it gets it right using random
 
-def guess():
-    computer_guesses = 5
+import random
+
+def guess(x):
+    low = 1 
+    high = x
+    computer_guesses = random.randint(low, high)
+
     while True:
         print('computer guessed =',computer_guesses)
         user = input("Enter if too low or too high or correct: ").lower()
 
         if user == "low":
-            computer_guesses += 1
-            print(computer_guesses)
+            low = computer_guesses + 1
+            computer_guesses = random.randint(low, high)
 
         elif user == "high":
-            computer_guesses -= 1
-            print(computer_guesses)
+            high = computer_guesses - 1
+            computer_guesses = random.randint(low, high)
         
         elif user == "correct":
             print("Guessed Correctly didn't I!!!!!!")
             break
 
 
-guess()
+guess(10)
