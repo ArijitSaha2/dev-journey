98d372e262c040c88db3e1746c11a4d128d057c9
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-16 13:21:16 +0530

    Day 14 - Revision Exercise 4: Sum of List

---
+++ 12_revision_topics_01_to_11/04_exercise.py
@@ -0,0 +1,10 @@
+# Create a list of 5 numbers
+# Print the sum of all numbers using a loop
+
+l = [1, 2, 3, 4, 5]
+# print(sum(l))
+
+s = 0
+for i in l: # 0 + 1 = 1 + 2 = 3 + 3 = 6 + 4 = 10 + 5
+    s += i
+print(s)
