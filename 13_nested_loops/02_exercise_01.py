aa23e401b890c4ed993eb722986aaa1892f86194
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-19 12:02:21 +0530

    Day 16 - Nested Loops Exercise 1

---
+++ 13_nested_loops/02_exercise_01.py
@@ -0,0 +1,10 @@
+# Use a nested loop
+# Outer loop runs 3 times
+# Inner loop runs 3 times
+# Print "* " each inner loop
+# Print new line after each outer loop
+
+for i in range(1, 4):
+    for y in range(1, 4):
+        print("*", end=" ")
+    print(" ")
