f72041427429238631b9f28a8fb9c3fd1b3b82a8
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-20 11:07:24 +0530

    Day 17 - Nested Loops Exercise 10: Fruit Letter Printer

---
+++ 13_nested_loops/11_exercise_10.py
@@ -0,0 +1,11 @@
+# Create a list of 3 fruits
+# Use a nested loop to print each letter of each fruit on separate lines
+# Print the fruit name before its letters
+
+l = ["Apple", "Banana", "Dragon"]
+
+for i in (l):
+    print(i)
+    for j in (i):
+        print(j)
+    print()
