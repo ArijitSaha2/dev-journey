3b3be79140ad5690d97021559913de8e47cfa6d6
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-22 20:29:21 +0530

    Day 19 - Nested Loops Exercise 15: Row-Based Number Output

---
+++ 13_nested_loops/16_exercise_15.py
@@ -0,0 +1,9 @@
+# Write a Python program using nested loops to print the following pattern:
+# 1 1 1
+# 2 2 2
+# 3 3 3
+
+for i in range(1,4):
+    for j in range(1,4):
+        print(i,end=" ")
+    print()
