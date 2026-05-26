b493d0f8eba9b728216ae8b67dafddfa23e58efd
Arijit Saha <Arijitsaha2334@gmail.com>
2026-05-26 12:00:26 +0530

    Day 22 - Iterables Notes

---
+++ 16_iterables/01_notes.py
@@ -0,0 +1,36 @@
+# Iterables = An object/collection that can return its elements one at a time, 
+#             allowing it to be iterated over in a loop
+
+# Iterables = anything you can loop through one element at a time
+# Lists, tuples, sets, strings, dictionaries are all iterables
+# When you use "for x in something" - that "something" must be an iterable
+
+'''
+# Tuple
+numbers = (1, 2, 3, 4, 5)
+
+for number in numbers:
+    print(number)
+
+
+# Set
+fruits = {"apple", "banana", "orange", "coconut"}
+
+for fruit in fruits:
+    print(fruit)
+
+
+# String
+name = "Bro Code"
+
+for character in name:
+    print(character, end=" ")
+
+
+
+# Dictionary
+my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+for key, value in my_dictionary.items():
+    print(f"{key} = {value}")
+'''
