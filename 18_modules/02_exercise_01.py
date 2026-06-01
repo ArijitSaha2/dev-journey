fedd241afdd49052c1cb5dc1abd8a65fb8991344
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-01 14:18:26 +0530

    Day 27 - Modules Exercise 1: calculator module

---
+++ 18_modules/02_exercise_01.py
@@ -0,0 +1,11 @@
+# Create a module called calculator.py with two functions:
+# add(a, b) and multiply(a, b)
+# Import it in this file and print the results of both functions
+
+import calculator
+
+result = calculator.add(10, 4)
+result2 = calculator.multiply(10, 4)
+
+print(result)
+print(result2)
