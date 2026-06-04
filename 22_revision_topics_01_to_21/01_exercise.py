399f7438d3c63362be78f3e480304e78b9ccaa7c
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-04 14:05:23 +0530

    Day 30 - Revision Exercise 1: Data Types & Variables

---
+++ 22_revision_topics_01_to_21/01_exercise.py
@@ -0,0 +1,16 @@
+# Revision - Data Types & Variables
+# Create variables of 4 different data types: int, float, string, boolean
+# Print each variable and its type using type()
+
+i = 5
+
+f = 5.
+
+s = "5"
+
+b = True
+
+print(f"Value {i} is type {type(i)}")
+print(f"Value {f} is type {type(f)}")
+print(f"Value {s} is type {type(s)}")
+print(f"Value {b} is type {type(b)}")
