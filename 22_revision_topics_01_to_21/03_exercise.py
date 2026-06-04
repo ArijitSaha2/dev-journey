b67a4b8f083d9cd3d646147db63d825fcfc515eb
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-04 14:18:20 +0530

    Day 30 - Revision Exercise 3: User Input & Arithmetic

---
+++ 22_revision_topics_01_to_21/03_exercise.py
@@ -0,0 +1,11 @@
+# Revision - User Input & Arithmetic
+# Ask the user for 2 numbers
+# Print their sum, difference, product, and remainder
+
+userin1 = int(input("Enter first number : "))
+userin2 = int(input("Enter second number: "))
+
+print(f"Sum of the numbers = {userin1 + userin2}")
+print(f"Difference of the numbers = {userin1 - userin2}")
+print(f"Product of the numbers = {userin1 * userin2}")
+print(f"Remainder of the numbers = {userin1 % userin2}")
