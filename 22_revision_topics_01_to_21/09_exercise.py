80e07e19d1a812a8eeda033ae6fd8dfcf8c78772
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-05 19:07:07 +0530

    Day 31 - Revision Exercise 9: String Indexing

---
+++ 22_revision_topics_01_to_21/09_exercise.py
@@ -0,0 +1,13 @@
+# Revision - String Indexing
+# name = "Backend Developer"
+# Print the first character
+# Print the last character
+# Print every other character using slicing
+# Print the string reversed
+
+name = "Backend Developer"
+
+print(name[0])
+print(name[-1])
+print(name[::2])
+print(name[::-1])
