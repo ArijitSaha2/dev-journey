989a7519d515483e086538618991ed134d8dc277
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-11 13:03:04 +0530

    Day 36 - Lambda Functions Notes

---
+++ 24_lambda_functions/01_notes.py
@@ -0,0 +1,26 @@
+# Lambda Function can have multiple parameters
+# Must be written on a single line
+# Have different syntax for the regular functions
+# No need for an explicit Return statement
+# Lambda functions don't have a name and they are also known as anonymous functions
+# EXAMPLE:
+# square = lambda num: num ** 2
+# addition = lambda num1, num2: (num1 + num2)
+# print(square(5))
+# print(addition(2, 2))
+
+
+def transform_list(nums_list, transform_item): 
+    transformed_0 = transform_item(nums_list[0])
+    transformed_1 = transform_item(nums_list[1])
+    return [transformed_0, transformed_1]
+
+print(transform_list([2, 3], lambda num: num ** 2))
+print(transform_list([2, 3], lambda num: num ** 3))
+
+
+
+nums_list = [2, 3, 4, 5, 6]
+print(list(map(lambda num: num ** 3, nums_list))) # map function
+
+print(list(filter(lambda num: num % 2 == 0, nums_list))) # filter function
