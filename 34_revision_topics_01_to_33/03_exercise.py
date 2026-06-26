6050885754cb626abdf5b64b00a90a73d19d774d
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-26 12:01:53 +0530

    Day 50 - Revision Exercise 3 - User Input

---
+++ 34_revision_topics_01_to_33/03_exercise.py
@@ -0,0 +1,10 @@
+# Revision Exercise 3
+# Take a user's name as input
+# Convert the name to uppercase
+# Print the uppercase name
+# Print the number of characters in the name
+
+name = input("Enter your name: ")
+
+print(name.upper())
+print(len(name))
