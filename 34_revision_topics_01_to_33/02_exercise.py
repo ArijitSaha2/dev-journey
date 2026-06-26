20dbe12430603fec0d42631cd6654c115360a576
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-26 11:59:42 +0530

    Day 50 - Revision Exercise 2 - Data Types

---
+++ 34_revision_topics_01_to_33/02_exercise.py
@@ -0,0 +1,17 @@
+# Revision Exercise 2
+# Create variables:
+# name = "Alex"
+# age = 20
+# height = 5.8
+# is_student = True
+# Print the data type of each variable
+
+name = "Alex"
+age = 20
+height = 5.8
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
