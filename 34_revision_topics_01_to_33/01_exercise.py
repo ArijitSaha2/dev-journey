9ad66f8dcd21fb3461a81204580ead65a8179f94
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-26 11:58:03 +0530

    Day 50 - Revision Exercise 1 - Operators

---
+++ 34_revision_topics_01_to_33/01_exercise.py
@@ -0,0 +1,23 @@
+# Revision Exercise 1
+# Create two variables:
+# a = 15
+# b = 4
+# Print:
+# Addition , Subtraction, Multiplication, Division, Floor Division, Modulus, Exponent
+
+a = 15
+b = 4
+
+print(f"Addition = {a + b}")
+
+print(f"Subtraction = {a - b}")
+
+print(f"Multiplication = {a * b}")
+
+print(f"Division = {a / b}")
+
+print(f"Floor Division = {a // b}")
+
+print(f"Modulus = {a % b}")
+
+print(f"Exponent = {a ** b}")
