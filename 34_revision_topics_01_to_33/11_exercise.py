95264dabef3e66d6e4b185efe5072796a320abdd
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-26 12:49:27 +0530

    Day 50 - Revision Exercise 11 & 12 - Dictionaries and Tuples

---
+++ 34_revision_topics_01_to_33/11_exercise.py
@@ -0,0 +1,11 @@
+# Revision Exercise 11
+# Create a dictionary with:
+# name = "Alex"
+# age = 20
+# city = "Mumbai"
+# Print each key and value using a for loop
+
+dictionary = {"name": "Alex", "age": 20, "city": "Mumbai"}
+
+for x, y in dictionary.items():
+    print(f"{x} : {y}")
---
+++ 34_revision_topics_01_to_33/12_exercise.py
@@ -0,0 +1,14 @@
+# Revision Exercise 12
+# Create a tuple containing 5 colors
+# Use a for loop to print each color
+# Print the total number of colors
+
+t = ("red", "green", "blue", "orange", "purple")
+
+total = 0 
+
+for x in t:
+    print(x)
+    total += 1
+
+print(f"Total Colors = {total}")
