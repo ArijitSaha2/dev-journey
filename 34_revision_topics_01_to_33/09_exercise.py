0bdf67496195a9785b74a069e477606c2156a651
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-26 12:37:24 +0530

    Day 50 - Revision Exercise 9 & 10 - Functions and Lists

---
+++ 34_revision_topics_01_to_33/09_exercise.py
@@ -0,0 +1,10 @@
+# Revision Exercise 9
+# Create a function called square()
+# It should take one number as a parameter
+# Return its square
+# Call the function and print the result
+
+def square(num):
+    return num * num
+
+print(f"Square = {square(12)}")
---
+++ 34_revision_topics_01_to_33/10_exercise.py
@@ -0,0 +1,12 @@
+# Revision Exercise 10
+# Create a list: [10, 20, 30, 40, 50]
+# Use a for loop to print each value
+# Print the total sum of the list
+
+li = [10, 20, 30, 40, 50]
+
+for x in li:
+
+    print(x)
+
+print(f"Total Sum = {sum(li)}")
