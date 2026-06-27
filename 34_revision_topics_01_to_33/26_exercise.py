c4b383b882b4bfc9a291c298fb8e2e96b28e4e6e
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-27 16:56:02 +0530

    Day 51 - Revision Exercise 24 to 27 - Lambda Functions

---
+++ 34_revision_topics_01_to_33/26_exercise.py
@@ -0,0 +1,10 @@
+# Revision Exercise 26
+# Create a lambda function called cube
+# It should take one number
+# Return its cube
+# Call it with 3
+# Print the result
+
+cube = lambda x: x**3
+
+print(cube(3))
---
+++ 34_revision_topics_01_to_33/27_exercise.py
@@ -0,0 +1,10 @@
+# Revision Exercise 27
+# Create a lambda function called starts_with_a
+# It should take one string
+# Return True if the string starts with "A", otherwise False
+# Call it with "Alex"
+# Print the result
+
+starts_with_a = lambda x: x.startswith("A")
+
+print(starts_with_a("Alex"))
