a89c1c26b19a09f9e712e9dea696b3197208e07b
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-27 16:28:22 +0530

    Day 51 - Revision Exercise 21 - Modules and if __name__ == "__main__"

--- 34_revision_topics_01_to_33/21_exercise.py
+++ 34_revision_topics_01_to_33/21_exercise.py
@@ -1,8 +1,9 @@
 # Revision Exercise 21
 # Create your own module called greetings.py
 # Inside it, create a function called say_hello(name)
+# Add an if __name__ == "__main__": block in greetings.py
 # Import the module into main.py
-# Call say_hello() and print the greeting
+# Call say_hello() from main.py
 
 from greetings import say_hello
 
--- 34_revision_topics_01_to_33/greetings.py
+++ 34_revision_topics_01_to_33/greetings.py
@@ -2,3 +2,6 @@
 
 def say_hello(name= "Guest"):
     print(f"Hello! {name}")
+
+if __name__ == "__main__":
+    say_hello("Kai")
