f30a9d9593f96fc0243255ecdcfde78a268660f6
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-27 16:15:19 +0530

    Day 51 - Revision Exercise 21 - Creating and Importing Modules

---
+++ 34_revision_topics_01_to_33/21_exercise.py
@@ -0,0 +1,9 @@
+# Revision Exercise 21
+# Create your own module called greetings.py
+# Inside it, create a function called say_hello(name)
+# Import the module into main.py
+# Call say_hello() and print the greeting
+
+from greetings import say_hello
+
+say_hello("Rose")
---
+++ 34_revision_topics_01_to_33/greetings.py
@@ -0,0 +1,4 @@
+# Module for 21_exercise.py
+
+def say_hello(name= "Guest"):
+    print(f"Hello! {name}")
