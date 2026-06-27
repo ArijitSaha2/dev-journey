43a4121f3e4850f5c02e2257cb5e41acb73366d8
Arijit Saha <Arijitsaha2334@gmail.com>
2026-06-27 16:51:38 +0530

    Day 51 - Revision Exercise 24 & 25 - Lambda Functions

---
+++ 34_revision_topics_01_to_33/24_exercise.py
@@ -0,0 +1,10 @@
+# Revision Exercise 24
+# Create a lambda function called square
+# It should take one number
+# Return its square
+# Call it with 9
+# Print the result
+
+square = lambda x: x * x
+
+print(square(9))
---
+++ 34_revision_topics_01_to_33/25_exercise.py
@@ -0,0 +1,10 @@
+# Revision Exercise 25
+# Create a lambda function called is_even
+# It should take one number
+# Return True if the number is even, otherwise False
+# Call it with 12
+# Print the result
+
+is_even = lambda x: x % 2 == 0
+
+print(is_even(12))
