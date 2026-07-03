a991ab29bceef9e834a6a15163c5dd3f61d121b4
Arijit Saha <Arijitsaha2334@gmail.com>
2026-07-03 11:43:22 +0530

    Day 56 - File Detection Notes

---
+++ 39_file_detection/01_notes.py
@@ -0,0 +1,22 @@
+# python file detection
+
+# Relative file path = Location of a file/folder relative to the current working directory.
+# Example: "stuff/test.txt"
+
+# Absolute file path = Complete location of a file/folder starting from the drive/root directory.
+# Example: "C:\\Users\\ariji\\OneDrive\\Desktop\\test" or "C:/Users/ariji/OneDrive/Desktop/test"
+
+import os
+
+file_path = "stuff/test.txt" # Relative Path
+
+if os.path.exists(file_path):
+    print(f"The location '{file_path}' exists")
+
+    if os.path.isfile(file_path):
+        print("That is a file")
+    elif os.path.isdir(file_path):
+        print("That is a directory")
+
+else:
+    print("That location doesn't exist")
