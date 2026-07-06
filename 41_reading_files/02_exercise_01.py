7f8a2665b079a7f97f84d9debc19cbb4f476cf05
Arijit Saha <Arijitsaha2334@gmail.com>
2026-07-06 13:17:59 +0530

    Day 59 - File Reading Exercise 1 - TXT, JSON and CSV Reading

--- 41_reading_files/02_exercise_01.py
+++ 41_reading_files/02_exercise_01.py
@@ -6,16 +6,35 @@
 
 # for .txt
 file_path = "story.txt"
-
-with open(file_path, "r") as file:
-    content = file.read()
-    print(content)
+print("For Txt file")
+try:
+    with open(file_path, "r") as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print("File not Found!")
 
 # for .json
 import json 
 
 file_path = "player.json"
+print("For Json file")
+try:
+    with open(file_path, "r") as file:
+        content = json.load(file)
+        print(content)
+except FileNotFoundError:
+    print("File not Found!!")
 
-with open(file_path, "r") as file:
-    content = json.load(file)
-    print(content)
+# for .csv
+import csv
+
+file_path = "scores.csv"
+print("For Csv file")
+try:
+    with open(file_path, "r") as file:
+        content = csv.reader(file)
+        for line in content:
+            print(line)
+except FileNotFoundError:
+    print("File not Found!!!")
